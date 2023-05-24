bcfbeeced51089bb378a19856ce63f26698cbbe0
Yuje Lee <dbwp031@gmail.com>
2023-05-24 14:49:15 +0900

    update 4장 practice

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -1,49 +1,24 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
+
+@Getter
+@Setter
 @Entity
-@Table(name="MEMBER")
 public class Member {
-    //Getter Setter
-
     @Id
-    @Column(name="ID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
-    @Column(name="NAME")
-    private String username;
-    //매핑 정보가 없는 필드
-    private Integer age;
+    @GeneratedValue
+    private Long id;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    private String name;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+    private String city;
+    private String street;
+    private String zipcode;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-    public String getId(){return id;}
-    public void setId(String id) {
-        this.id = id;}
-
-    public String getUsername(){
-        return username;}
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
