14690769d816693fe54cb8863b7b23f5b425df3a
Yuje Lee <dbwp031@gmail.com>
2023-05-24 13:49:47 +0900

    update

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -1,4 +1,49 @@
 package org.example;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="MEMBER")
 public class Member {
+    //Getter Setter
+
+    @Id
+    @Column(name="ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column(name="NAME")
+    private String username;
+    //매핑 정보가 없는 필드
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+    public String getId(){return id;}
+    public void setId(String id) {
+        this.id = id;}
+
+    public String getUsername(){
+        return username;}
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
